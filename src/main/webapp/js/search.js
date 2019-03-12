338ee3b5eabb7bca2c2329db45e0fed11d70ebfd
sergpank <sergpank@gmail.com>
2019-03-12 02:02:22 +0200

    call search function by hitting enter

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -7,10 +7,9 @@ function searchStreets(ev)
     if (this.readyState == 4 && this.status == 200)
     {
       var response = JSON.parse(this.responseText);
-      var responseText = "";
 
-      responseText += "<table>";
-      responseText += "<tr> <td>ID</td> <td>NAME</td> <td>NAME_RU</td> <td>NAME_OLD</td> </tr>"
+      var htmlResponse = "<table>";
+      htmlResponse += "<tr> <td>ID</td> <td>NAME</td> <td>NAME_RU</td> <td>NAME_OLD</td> </tr>"
 
       for (var i = 0; i < response.length; i++)
       {
@@ -20,20 +19,17 @@ function searchStreets(ev)
         var nameRu = element.nameRu == undefined ? "N/A" : element.nameRu;
         var nameOld = element.nameOld == undefined ? "N/A" : element.nameOld;
 
-        responseText += "<tr> <td>" + id + "</td> <td>" + name + "</td> <td>" + nameRu + "</td> <td>" + nameOld + "</td> </tr>"
+        htmlResponse += "<tr> <td>" + id + "</td> <td>" + name + "</td> <td>" + nameRu + "</td> <td>" + nameOld + "</td> </tr>"
       }
-
-      responseText += "</table>";
-
-      document.getElementById("demo").innerHTML = responseText;
-      ev.stopPropagation();
+      htmlResponse += "</table>";
+      document.getElementById("demo").innerHTML = htmlResponse;
     }
   };
 
   var key = document.getElementById("key").value;
 
-  xhttp.open("GET", "http://localhost:8080/onemap/search?key=" + key, true);
-
+  xhttp.open("GET", "http://onemap.md/search?key=" + key, true);
+//  xhttp.open("GET", "http://localhost:8080/onemap/search?key=" + key, true);
   xhttp.send();
 };
 
@@ -45,14 +41,9 @@ function loadDoc()
   xhttp.onreadystatechange = function()
   {
     if (this.readyState == 4 && this.status == 200)
-    {
-      // key for form request - just for test
-      var key = document.getElementsByName("key")[0].value;
-      document.getElementById("demo").innerHTML = this.responseText + "[" + key + "]";
-    }
+      document.getElementById("demo").innerHTML = this.responseText ;
   };
 
   xhttp.open("GET", "ajax_info.txt", true);
-
   xhttp.send();
 };
